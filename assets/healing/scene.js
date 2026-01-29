836e362139d458873445db9b5a62201b79c5bd61
IAstriel <violetemu89@gmail.com>
2026-01-28 22:50:13 -0400

    Decrease audio volume from 0.35 to 0.28

--- assets/healing/scene.js
+++ assets/healing/scene.js
@@ -247,7 +247,7 @@ export function createMusicController(buttonEl, tracks){
 
   const audio = new Audio(tracks[musicIndex].src);
   audio.loop = true;
-  audio.volume = 0.35;
+  audio.volume = 0.28;
 
   function setBtnState(){
     if(!buttonEl) return;
